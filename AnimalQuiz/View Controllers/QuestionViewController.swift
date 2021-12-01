00d21b0876a67d6ea5657c93c6fa0a041a546dc5
mekkers <mekkers>
2021-12-01 14:54:24 +0300

    Initial Commit

---
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by igor mekkers on 01.12.2021.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
